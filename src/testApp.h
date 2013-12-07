1d5208b52af102bfcd873a8e43bab848db7a10d1
Andrew McWilliams <andy@jahya.net>
2013-12-07 16:47:20 -0500

    Switches to ofxNI2 user tracking example

--- src/testApp.h
+++ src/testApp.h
@@ -1,35 +1,29 @@
-#ifndef _TEST_APP
-#define _TEST_APP
+#pragma once
 
-#include "ofxOpenNI.h"
 #include "ofMain.h"
 
-#define MAX_DEVICES 2
+#include "ofxNI2.h"
+#include "ofxNiTE2.h"
 
-class testApp : public ofBaseApp{
-
+class testApp : public ofBaseApp
+{
 public:
-    
 	void setup();
+	void exit();
+	
 	void update();
 	void draw();
-    void exit();
-    
-	void keyPressed  (int key);
+
+	void keyPressed(int key);
 	void keyReleased(int key);
-	void mouseMoved(int x, int y );
+	void mouseMoved(int x, int y);
 	void mouseDragged(int x, int y, int button);
 	void mousePressed(int x, int y, int button);
 	void mouseReleased(int x, int y, int button);
 	void windowResized(int w, int h);
-
-    int numDevices;
-	ofxOpenNI openNIDevices[MAX_DEVICES];
-    
-    ofTrueTypeFont verdana;
-    
-    void userEvent(ofxOpenNIUserEvent & event);
-    
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+	
+	ofxNI2::Device device;
+	ofxNiTE2::UserTracker tracker;
 };
-
-#endif
